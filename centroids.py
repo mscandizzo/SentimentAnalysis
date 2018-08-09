ffcd2e3d62f1752caf56da26702889181c790fce
spartners <mariano.scandizzo@spartners.me>
2018-08-09 17:33:16 -0400

    v11

--- centroids.py
+++ centroids.py
@@ -1,9 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import seaborn as sns; sns.set()
-
-# Set up Hyperparameters 
-max_interactions = 5000
+from copy import deepcopy
 
 def create_sample(k=3,maximizer=6,datapoints=100):
     vectores = np.array([])
@@ -15,33 +13,49 @@ def create_sample(k=3,maximizer=6,datapoints=100):
         colores = np.append(colores, color)
     vectores = np.vstack(vectores).reshape((100*k,2))
     colores = np.hstack(colores)
-    return vectores, colores
+    return vectores, colores, k
 
 def get_random_centroids(vectores, k):
     return vectores[np.random.permutation(len(vectores))[0:k]]
 
 def dist(a,b, ax=1):
-    return np.linalg.norm(a - b, axis =ax)
+    return np.linalg.norm(a - b, axis=ax)
 
 def get_nearest_centroid_for_all_points(vectores,centroids):
-    clusters = np.zeros(len(vectores))
+    closest = np.zeros(len(vectores))
     for i in range(len(vectores)):
         distances = dist(vectores[i],centroids)
         cluster = np.argmin(distances)
-        clusters[i] = cluster
-    return clusters
+        closest[i] = cluster
+    return closest
 
 def move_centroids(vectores,closest,centroids):
     for k in range(centroids.shape[0]):
-        cluster = points[closest==k]
+        cluster = vectores[closest==k]
         centroids[k] = cluster.mean(axis=0)
     return centroids
 
-last_closest =[]
-for i in range(max_interactions):
+def looping_centroids(vectores, centroids,max_iteractions=5000):
+    last_closest =[]
+    movimiento = []
+    print(centroids)
+    movimiento.append(centroids)
+    for i in range(max_iteractions):
+        closest = get_nearest_centroid_for_all_points(vectores,centroids)
+        centroids = move_centroids(vectores,closest,centroids)
+        print(centroids)
+        movimiento.append(deepcopy(centroids))
+        if (np.array_equal(last_closest,closest)):
+            break
+        last_closest = closest
+    
+    return movimiento 
+
+
+def run_process():
+    vectores, colores, k= create_sample()
+    centroids = get_random_centroids(vectores,k)
     closest = get_nearest_centroid_for_all_points(vectores,centroids)
-    centroids = move_centroids(vectores,closest,centroids)
-    if (np.array_equal(last_closest,closest)):
-        print('Clusters converged in %d iterations' % i)
-        break
-    last_closest = closest
+    movimiento =looping_centroids(vectores,centroids,closest)
+    return movimiento
+
